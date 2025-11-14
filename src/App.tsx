da99b282429aeb84d964c76c3af44802af89bd10
Two Four <qolu02@gmail.com>
2025-11-14 01:28:21 -0500

    Hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from '@/routes/home/HomePage';
 import { ShopPage } from '@/routes/shop/ShopPage';
 import { DashboardPage } from '@/routes/dashboard/DashboardPage';
 
 function App() {
   return (
-    <BrowserRouter basename="/figma_mcp_experiment_2">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
